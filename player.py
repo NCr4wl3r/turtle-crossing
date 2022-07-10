a54d6a4e31836b68ad706c11a76d047d91d985fa
Espartaco Rodriguez Barquero <espar-rodbar@MacBook-Air-de-Espar.local>
2022-07-10 10:13:26 +0200

    implemented turtle class

--- player.py
+++ player.py
@@ -1,7 +1,17 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self, shape="turtle") -> None:
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        new_position = self.ycor() + MOVE_DISTANCE
+        self.sety(new_position)
