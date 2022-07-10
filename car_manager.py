813c675df15b25169322251eed79c17e112841c4
Espartaco Rodriguez Barquero <espar-rodbar@MacBook-Air-de-Espar.local>
2022-07-10 10:47:31 +0200

    Implemented car class

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+from random import choice, randint
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self, lvl) -> None:
+        super().__init__()
+        self.shape("square")
+        self.color(choice(COLORS))
+        self.setheading(180)
+        self.setx(300)
+        self.set_speed(lvl)
+
+    def move(self):
+        self.forward(self.move_distance)
+
+    def set_starting_y_pos(self):
+        self.sety(randint(-250, 250))
+
+    def set_speed(self, lvl=1):
+        self.move_distance = STARTING_MOVE_DISTANCE + MOVE_INCREMENT * (lvl - 1)
